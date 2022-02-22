5599de10b82c5b7f43b4fa9acd53d9facc0bd4d9
Pietro Grandi <pietrograndi@gmail.com>
2022-02-23 00:39:39 +0100

    progess home

--- src/pages/users.tsx
+++ src/pages/users.tsx
@@ -1,20 +1,27 @@
-import { useNavigate } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import { RouteEnum } from '../routes/routes'
 import { Button } from '../components/button/button'
-import { useAppSelector } from '../reducers'
+import { RootState, useAppSelector } from '../reducers'
 
 
 export function Users() {
-  const users = useAppSelector(({users}) => Object.values(users))
+  const users = useAppSelector(({users}:RootState) => Object.values(users))
   const navigate = useNavigate()
-
 
   return (
     <div>
       Users page
       <Button
         click={() => navigate(RouteEnum.NEW_USERS)} text="create new user"/>
-      {users.length === 0 && <div>no users :(</div>}
+      { users.length === 0 && <div>no users :(</div>}
+      <div>
+
+      { users.length > 0 &&
+      <ul>
+        {users.map((user) => <li><Link  to={`${RouteEnum.USER}/${user.id}`} >{user.name} - {user.friends.length}</Link></li>)}
+      </ul>
+      }
+      </div>
     </div>
   )
 }
